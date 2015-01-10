ae283479b9798d6d0ffa5c9f1ff3713e717576de
npow <nissan.pow@gmail.com>
2015-01-09 23:50:14 -0500

    use getopt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <unistd.h>
 #include <State.h>
 
-int main() {
-  State s(5, 4);
-//  State s(7, 6);
+int main(int argc, char* const argv[]) {
+  bool isWhite = true;
+  bool isSmallBoard = true;
+  char c = '\0';
+  while ((c = getopt(argc, argv, "bl")) != -1) {
+    switch (c) {
+      case 'b':
+        isWhite = false;
+        break;
+      case 'l':
+        isSmallBoard = false;
+        break;
+    }
+  }
+  cout << "isWhite: " << isWhite << endl;
+  cout << "isSmallBoard: " << isSmallBoard << endl;
+
+  int width = 5;
+  int height = 4;
+  if (!isSmallBoard) {
+    width = 7;
+    height = 6;
+  }
+
+  State s(width, height);
   s.print();
   return 0;
 }
